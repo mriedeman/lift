5fc374369b53b70a16e4baa186d3651cca1996ab
michael <riedemanmj@gmail.com>
2021-10-17 12:12:07 -0700

    initial entry form model

--- lift/website/models.py
+++ lift/website/models.py
@@ -1,94 +1,67 @@
 from django.db import models
-from django.db.models.fields import AutoField
+
 
 # Create your models here.
 
 
 class Pipes(models.Model):
     
-   
+    #hazen-williams equation
+    #pipe_length (feet)
+    #flow_rate (gal/min)
+    #inner diamter (inches)
 
 
-    #frontend
-    length = models.input_form   #find out how to define a input range
-    material = MATERIAL_CHOICES = [ #double check this form input syntax
-    ('pvc', '.09'), #ROUGHNESS
-    ('copper', '.01'),
-    ('carbon_steel', '.1'),
-    ('carbon_steel', '.1'),
-    ('carbon_steel', '.1'),
-    ('carbon_steel', '.1'),
-    ('carbon_steel', '.1'),
-    ]
+    #PIPE_LENGTH
 
-    diameter = DIAMETER_CHOICES = [
-    ('pvc', '.75'), #DIAMETER IN INCHES  #determine how to make choices integers
-    ('copper', '1'),
-    ('carbon_steel', '3'),
-    ('carbon_steel', '4'),
-    ('carbon_steel', '6'),
-    ('carbon_steel', '8'),
-    ('carbon_steel', '10'),
-    ('carbon_steel', '12'),
-    ('carbon_steel', '16'),
-    ('carbon_steel', '18'),
-    ('carbon_steel', '24'),
-    ('carbon_steel', '30'),
-    ]
+    pipe_length = models.DecimalField(max_digits = 8, decimal_places=2)
 
 
-    schedule = [
-          ('carbon_steel', '3'),
-    ('schedule 40', '1'),
-    ('schedule 40', '1'),
-    ('schedule 40', '1'),
-    ('schedule 40', '1'),
-    ('schedule 40', '1'),
-    ('schedule 40', '1'),
-    ('schedule 40', '1'),
-    ('schedule 40', '1'),
-    ('schedule 40', '1'),
-    ]
+    MATERIALS =  [
+        (130, 'Acrylonite Butadiene Styrene'),
+        (190, 'Aluminum'),
+        (140, 'Asbestos Cement')
+]
+    material = models.CharField(max_length = 100, choices = MATERIALS, default = 'Aluminum')
 
-
-     #backend
+    inner_diameter = models.DecimalField(max_digits = 8, decimal_places=2)
     
-    #need to learn how to call object from db and use its attributes/methods
+
+    OUTLET = [
+        ('suc', 'suction'),
+        ('dis', 'discharge')
+    ]
+
+    outlet_type = models.CharField(max_length = 100, choices = OUTLET, default = 'dis')
     
-    name = f"{diameter} + {material} pipe" #this is fine
+    name = f"{inner_diameter} {material} pipe" 
 
 
     def __str__(self):
         return self.name
 
 
-# returns a dictionary that can be referenced 
-try:
-    go = pipe_specs = Pipes.objects.get()
-except Pipes.DoesNotExist:
-    go = None
-
 
 #processsing a form from a field
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+#from django.http import HttpResponseRedirect
+#from django.shortcuts import render
 
-from .forms import NameForm
+#from .forms import NameForm
 
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+# def get_name(request):
+#     # if this is a POST request we need to process the form data
+#     if request.method == 'POST':
+#         # create a form instance and populate it with data from the request:
+#         form = NameForm(request.POST)
+#         # check whether it's valid:
+#         if form.is_valid():
+#             # process the data in form.cleaned_data as required
+#             # ...
+#             # redirect to a new URL:
+#             return HttpResponseRedirect('/thanks/')
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
+#     # if a GET (or any other method) we'll create a blank form
+#     else:
+#         form = NameForm()
 
-    return render(request, 'name.html', {'form': form})
+#     return render(request, 'name.html', {'form': form})
