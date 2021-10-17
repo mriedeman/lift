8835533b801df3c67d6a241d1f69b5361b7fdc12
michael <riedemanmj@gmail.com>
2021-10-17 10:50:59 -0700

    notes on form entry

--- lift/website/models.py
+++ lift/website/models.py
@@ -61,3 +61,34 @@ class Pipes(models.Model):
     def __str__(self):
         return self.name
 
+
+# returns a dictionary that can be referenced 
+try:
+    go = pipe_specs = Pipes.objects.get()
+except Pipes.DoesNotExist:
+    go = None
+
+
+#processsing a form from a field
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import NameForm
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
